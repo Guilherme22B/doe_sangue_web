d2cb181a4cfccc62a9aafdaf1cdef327e98756cb
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2025-11-20 22:11:59 -0300

    refactor: mudando estrutura para o botão secundário

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 defineProps<{
     label: string;
-    click?: () => void
+    click?: () => void;
+    secondary?: boolean;
+    redFontColor?: boolean;
 }>();
 </script>
 
 <template>
-    <button class="poppins-medium">{{ label }}</button>
+    <button :class="{ 'btn-secondary': secondary, 'red-font-color': redFontColor }" class="poppins-medium">{{ label }}</button>
 </template>
+
 <style scoped>
 button {
     width: 100%;
@@ -24,6 +27,22 @@ button:hover {
     transition: 0.5s;
     cursor: pointer;
     background-color: var(--secondary-color);
+}
+
+.btn-secondary {
+    background-color: var(--button-secondary);
+    color: var(--font-color);
+    border: 1px solid;
+    border-color: var(--input-border-color);
+}
+
+.btn-secondary:hover {
+    background-color: var(--button-primary);
+    color: var(--font-button-primary);
+}
+
+.red-font-color {
+    color: var(--primary-color);
 }
 </style>
 
